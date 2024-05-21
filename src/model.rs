32cc90f5a88163c0eba7709e632775c8d907546e
Akash Panda <akashajaypanda@gmail.com>
2024-05-21 08:48:23 -0700

    initial model and started formatter

--- src/model.rs
+++ src/model.rs
@@ -1,121 +1,92 @@
 #![allow(unused)]
 // TODO: Remove this
 
+use statements::Stmt;
 use wabbit_proc_macros::{Expression, Statement};
 
 pub struct Program {
-    pub stmts: Vec<Box<dyn Statement>>,
+    pub stmts: Vec<Stmt>,
 }
 
-// pub trait Visitable<T> {
-//     fn accept(&self, visitor: &dyn Visitor<T>) -> T;
-// }
-
-pub trait Expression {
-    fn accept_transform(&self, transformer: &dyn Transformer) -> Box<dyn Expression>;
-}
-pub trait Statement {
-    fn accept_transform(&self, transformer: &dyn Transformer) -> Box<dyn Statement>;
-}
-
-pub trait Transformer {
-    fn visit_binop(&self, binop: &BinOp) -> Box<dyn Expression>
-    {
-        let left = binop.left.accept_transform(self);
-        let right = binop.right.accept_transform(self);
-
-        Box::new(BinOp {
-            op: binop.op,
-            left,
-            right,
-        })
+mod statements {
+    use super::expressions::{RelOp, Variable};
+    pub enum Stmt {
+        Func(Func),
+        If(If),
+        While(While),
     }
 
-    // fn visit_number(&self, number: &Number) -> T;
-    // fn visit_print(&self, print: &Print<T>) -> T;
-    // fn visit_return(&self, ret: &Return<T>) -> T;
-    // fn visit_if(&self, r#if: &If<T>) -> T;
-    // fn visit_while(&self, r#while: &While<T>) -> T;
-    // fn visit_relop(&self, relop: &RelOp<T>) -> T;
-    // fn visit_funccall(&self, funccall: &FuncCall<T>) -> T;
-    // fn visit_variable(&self, variable: &Variable) -> T;
-    // fn visit_func(&self, variable: &Func<T>) -> T;
-}
+    pub struct Func {
+        name: String,
+        args: Vec<Variable>,
+        body: Vec<Stmt>,
+    }
 
-// #[derive(Statement)]
-// pub struct Print<T> {
-//     pub value: Box<dyn Expression<T>>,
-// }
+    pub struct If {
+        condition: RelOp,
+        consequence: Vec<Stmt>,
+        alternative: Vec<Stmt>,
+    }
 
-// #[derive(Statement)]
-// pub struct Return<T> {
-//     expr: Box<dyn Expression<T>>,
-// }
-
-// #[derive(Statement)]
-// pub struct Func<T> {
-//     name: String,
-//     args: Vec<Variable>,
-//     body: Vec<Box<dyn Statement<T>>>,
-// }
-
-// #[derive(Expression)]
-// pub struct FuncCall<T> {
-//     name: String,
-//     args: Vec<Box<dyn Expression<T>>>,
-// }
-
-// #[derive(Expression)]
-// pub struct Variable {
-//     var: String,
-// }
-
-// #[derive(Statement)]
-// pub struct If<T> {
-//     condition: RelOp<T>,
-//     consequence: Vec<Box<dyn Statement<T>>>,
-//     alternative: Vec<Box<dyn Statement<T>>>,
-// }
-
-// #[derive(Statement)]
-// pub struct While<T> {
-//     condition: RelOp<T>,
-//     body: Vec<Box<dyn Statement<T>>>,
-// }
-
-// #[derive(Expression)]
-// pub enum Number {
-//     Int(isize),
-// }
-
-// #[derive(Expression)]
-pub struct BinOp {
-    pub op: Operation,
-    pub left: Box<dyn Expression>,
-    pub right: Box<dyn Expression>,
-}
-
-impl Expression for BinOp {
-    fn accept_transform(&self, visitor: &dyn Transformer) -> Box<dyn Expression> {
-        visitor.visit_binop(self)
+    pub struct While {
+        condition: RelOp,
+        body: Vec<Stmt>,
     }
 }
 
-// #[derive(Expression)]
-// pub struct RelOp<T> {
-//     pub op: RelOperation,
-//     pub left: Box<dyn Expression<T>>,
-//     pub right: Box<dyn Expression<T>>,
-// }
+mod expressions {
+    pub enum Expr {
+        Print(Print),
+        Return(Return),
+        FuncCall(FuncCall),
+        Variable(Variable),
+        BinOp(BinOp),
+        RelOp(RelOp),
+        Number(Number),
+    }
 
-#[derive(Clone, Copy)]
-pub enum Operation {
-    Add,
-    Mul,
-}
+    pub struct Print {
+        pub value: Box<Expr>,
+    }
 
-#[derive(Clone, Copy)]
-pub enum RelOperation {
-    Equal,
-    LessThan,
+    pub struct Return {
+        expr: Box<Expr>,
+    }
+
+    pub struct FuncCall {
+        name: String,
+        args: Vec<Expr>,
+    }
+
+    pub struct Variable {
+        var: String,
+    }
+
+    pub enum Number {
+        Int(isize),
+    }
+
+    pub struct BinOp {
+        pub op: Operation,
+        pub left: Box<Expr>,
+        pub right: Box<Expr>,
+    }
+
+    pub struct RelOp {
+        pub op: RelOperation,
+        pub left: Box<Expr>,
+        pub right: Box<Expr>,
+    }
+
+    #[derive(Clone, Copy)]
+    pub enum Operation {
+        Add,
+        Mul,
+    }
+
+    #[derive(Clone, Copy)]
+    pub enum RelOperation {
+        Equal,
+        LessThan,
+    }
 }
